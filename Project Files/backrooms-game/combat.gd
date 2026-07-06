2c7b6f37cb8ec5a3377ce216963a9ec6209eb727
CelestePlayer <296255909+CelestePlayer@users.noreply.github.com>
2026-07-06 12:05:37 -0400

    upload

--- Project Files/backrooms-game/combat.gd
+++ Project Files/backrooms-game/combat.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var turn = 0
-
+var player_health = str($Player.health)
+var enemy_health = str($enemy.health)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$Player/Label.text = player_health
+	$enemy/Label.text = enemy_health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,13 +15,11 @@ func _process(delta: float) -> void:
 		turn = 0
 		await get_tree().create_timer(3).timeout
 		$Player.health -= randi_range(10,15) * $enemy.attack / $Player.defense
-		var health_text = str($Player.health)
-		$Player/Label.text = health_text
+		$Player/Label.text = player_health
 		$Button.disabled = false
 
 func _on_button_pressed() -> void:
 	$enemy.health -= $Player.attack * 10 / 3 - $enemy.defense
-	var health_text = str($enemy.health)
-	$enemy/Label.text = health_text
+	$enemy/Label.text = enemy_health
 	turn += 1
 	$Button.disabled = true
