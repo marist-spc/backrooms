81172a6c338d527d2c72dcd76e8bb6c02f7c22e3
CelestePlayer <296255909+CelestePlayer@users.noreply.github.com>
2026-07-06 16:58:37 -0400

    Added item functionality

--- Project Files/backrooms-game/combat.gd
+++ Project Files/backrooms-game/combat.gd
@@ -9,8 +9,9 @@ func _ready() -> void:
 	$enemy/Label.text = enemy_health
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if turn == 1 and $enemy.health > 0:
 		turn = 0
 		await get_tree().create_timer(3).timeout
@@ -19,6 +20,7 @@ func _process(delta: float) -> void:
 		$Player/Label.text = player_health
 		$Attack.disabled = false
 		$Block.disabled = false
+		$Item.disabled = false
 
 func _on_button_pressed() -> void:
 	$enemy.health -= $Player.attack * 10 / 3 - $enemy.defense
@@ -27,13 +29,40 @@ func _on_button_pressed() -> void:
 	turn += 1
 	$Attack.disabled = true
 	$Block.disabled = true
+	$Item.disabled = true
 
 func _on_block_pressed() -> void:
 	$Attack.disabled = true
 	$Block.disabled = true
+	$Item.disabled = true
 	await get_tree().create_timer(3).timeout
 	$Player.health -= randi_range(10,15) * $enemy.attack / ($Player.defense * 3)
 	player_health = str($Player.health)
 	$Player/Label.text = player_health
 	$Attack.disabled = false
 	$Block.disabled = false
+	$Item.disabled = false
+
+
+func _on_item_pressed() -> void:
+	$Item.visible = false
+	$Item.disabled = true
+	if $Player.Inventory["almond water"] > 0:
+		$"Almond Water".visible = true
+		$"Almond Water".disabled = false
+		
+	else:
+		$None.visible = true
+
+func _on_almond_water_pressed() -> void:
+	$Player.health += 20
+	if $Player.health > $Player.max_health:
+		$Player.health = $Player.max_health
+	player_health = str($Player.health)
+	$Player/Label.text = player_health
+	$"Almond Water".disabled = true
+	$"Almond Water".visible = false
+	$Item.visible = true
+	$Attack.disabled = true
+	$Block.disabled = true
+	turn += 1
--- Project Files/backrooms-game/player.gd
+++ Project Files/backrooms-game/player.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+var weapon = {}
+var armor = {}
+var Inventory = {"almond water": 5, "item": 0, "another fucking item idk": 0}
 var health = 100
 var max_health = 100
+var SP = 10
+var max_SP = 10
 var attack = 5
 var defense = 3
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
