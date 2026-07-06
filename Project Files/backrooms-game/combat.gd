bf0ce19031fd2bf528d4ed596f769ba8cf2a4d5c
CelestePlayer <296255909+CelestePlayer@users.noreply.github.com>
2026-07-06 14:19:24 -0400

    fixed issue with accessing nodes and added block

--- Project Files/backrooms-game/combat.gd
+++ Project Files/backrooms-game/combat.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 var turn = 0
-var player_health = str($Player.health)
-var enemy_health = str($enemy.health)
+@onready var player_health = str($Player.health)
+@onready var enemy_health = str($enemy.health)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Player/Label.text = player_health
@@ -15,11 +15,25 @@ func _process(delta: float) -> void:
 		turn = 0
 		await get_tree().create_timer(3).timeout
 		$Player.health -= randi_range(10,15) * $enemy.attack / $Player.defense
+		player_health = str($Player.health)
 		$Player/Label.text = player_health
-		$Button.disabled = false
+		$Attack.disabled = false
+		$Block.disabled = false
 
 func _on_button_pressed() -> void:
 	$enemy.health -= $Player.attack * 10 / 3 - $enemy.defense
+	enemy_health = str($enemy.health)
 	$enemy/Label.text = enemy_health
 	turn += 1
-	$Button.disabled = true
+	$Attack.disabled = true
+	$Block.disabled = true
+
+func _on_block_pressed() -> void:
+	$Attack.disabled = true
+	$Block.disabled = true
+	await get_tree().create_timer(3).timeout
+	$Player.health -= randi_range(10,15) * $enemy.attack / ($Player.defense * 3)
+	player_health = str($Player.health)
+	$Player/Label.text = player_health
+	$Attack.disabled = false
+	$Block.disabled = false
