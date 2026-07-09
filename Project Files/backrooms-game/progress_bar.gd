b80609bfd95c0788fa5eaa18688ac1ade6a9486b
averyconfusedperson924-pixel <averyconfusedperson924@gmail.com>
2026-07-09 10:40:49 -0400

    Health Bar Updates

--- Project Files/backrooms-game/progress_bar.gd
+++ Project Files/backrooms-game/progress_bar.gd
@@ -1,9 +1,1 @@
 extends ProgressBar
-
-
-func _on_player_health_var() -> void:
-	pass # Replace with function body.
-
-
-func _on_player_max_health_var() -> void:
-	pass # Replace with function body.
